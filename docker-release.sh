71cb08876bd502e4e4a6e23fa4fcecf5d5c67559
Stéphane <148355601+auvents-brave@users.noreply.github.com>
2025-10-23 03:28:07 +0200

    little up

--- docker-release.sh
+++ docker-release.sh
@@ -1,5 +1,13 @@
+
+# create container and mount working directory
 docker run -dit --name stheno-build -v "$PWD":/work -w /work swift:latest bash
+
+# build library in relase configuration
 docker exec -it stheno-build bash -lc "swift build -c release"
+
+# copy built library to host machine
 docker cp stheno-build:/work/.build/release/libStheno.a ~/Downloads/
-# docker stop stheno-build && docker rm stheno-build
-edited
+
+# cleann up
+docker stop stheno-build
+docker rm stheno-build
