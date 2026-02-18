06e2f5a7f197f4a7834ec53aa8c5f05b9967b9e1
auvents-brave <auvents-brave03@icloud.com>
2026-02-18 09:32:22 +0100

    win

--- baseimage.sh
+++
@@ -1,6 +0,0 @@
-# docker run --rm docker.io/library/ubuntu:questing bash
-
-docker build -f Dockerfile -t steffff/swift:latest .
-docker tag steffff/swift:latest swift:latest
-docker push steffff/swift:latest
-
