e94ae125594559415471f41fa810a43da8798b1b
Stéphane <148355601+auvents-brave@users.noreply.github.com>
2026-06-10 19:46:48 +0200

    Add isTestFlight detection helper

--- Sources/Stheno/Misc.swift
+++ Sources/Stheno/Misc.swift
@@ -4,3 +4,8 @@ internal import Foundation
 public var isRunningInPreviews: Bool {
 	ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
 }
+
+/// Indicates whether the app is running via TestFlight.
+public var isTestFlight: Bool {
+	Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt"
+}
